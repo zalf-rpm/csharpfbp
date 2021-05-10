bb917db599c3b6bb14dfdd7d15153e53767435c4
Michael Berg-Mohnicke <michael.berg-mohnicke@zalf.de>
2021-05-10 19:45:06 +0200

    dev dump

---
+++ MonicaFlow/Components/CapabilityNetwork.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FBPLib;
+
+namespace Components
+{
+    public abstract class CapabilityNetwork : Network, IDisposable
+    {
+        public Mas.Infrastructure.Common.ConnectionManager ConMan = new();
+
+        // To detect redundant calls
+        private bool _disposed = false;
+
+        public void Dispose() => Dispose(true);
+
+        // Protected implementation of Dispose pattern.
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed) return;
+            if (disposing) ConMan?.Dispose();
+            _disposed = true;
+        }
+    }
+}
---
+++ MonicaFlow/MonicaFlow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using FBPLib;
+using Components;
+using System.Threading.Tasks;
+
+namespace MonicaFlow
+{ 
+    public class MonicaFlow : CapabilityNetwork
+    {
+        string description = "Click anywhere on selection area";
+        public override void Define()
+        {
+            Component("CapSrc", typeof(CapabilitySource));
+            Component("CapUse", typeof(CapabilityUse));
+            Component("WriteConsole", typeof(WriteToConsole));
+            Initialize("capnp://localhost:11002", Component("CapSrc"), Port("SR"));
+            Connect(Component("CapSrc"), Port("OUT"), Component("CapUse"), Port("IN"));
+            Connect(Component("CapUse"), Port("OUT"), Component("WriteConsole"), Port("IN"));
+            Initialize("Mas.Rpc.Climate.ITimeSeries", Component("CapSrc"), Port("CAPTYPE"));
+        }
+
+        static async Task Main(String[] argv)
+        {
+            using var network = new MonicaFlow();
+            await network.GoA();
+            Console.WriteLine("bla");
+        }
+    }
+}
